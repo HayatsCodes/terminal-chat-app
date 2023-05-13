b28bd99b1b1bfbf2cbb14bb02cff3d5bb667a34c
Hayatudeen Abdulrahman <104792493+HayatsCodes@users.noreply.github.com>
2023-05-13 16:58:49 +0000

    restructured the folders

--- client/commander.js
+++ client/commander.js
@@ -8,68 +8,44 @@ const program = new Command();
 program.version('1.0.0').description('Terminal Chat App');
 
 // Define the option choices
-const options = [
-  { name: 'Create-Chat-Room', value: 'Create-Chat-Room', message: 'Create a Chat Room' },
-  { name: 'Join-Chat-Room', value: 'Join-Chat-Room', message: 'Join a Chat Room' },
-  { name: 'Exit', value: 'Exit', message: 'Exit the App' },
-];
-
-// Start Terminal chat app
-program.command('start').action(async () => {
-  let exit = false;
-
-
-
-  // when the app starts a list of option should be shown that prompts user
-  const { selectedOption } = await prompt({
+const homeOptions = [
+  {
     type: 'list',
     name: 'selectedOption',
     message: 'Home',
-    choices: [...options],
-  });
+    choices: [
+      { name: 'Create-Chat-Room', value: 'Create-Chat-Room', message: 'Create a Chat Room' },
+      { name: 'Join-Chat-Room', value: 'Join-Chat-Room', message: 'Join a Chat Room' },
+      { name: 'Exit', value: 'Exit', message: 'Exit the App' },
+    ]
+  },
+
+];
+
+
+// Start Terminal chat app
+program
+.description('Starts the Terminal chat app')
+.command('start').action(async () => {
+  let exit = false;
+
+
+  // Prompt user, when app starts
+  const { selectedOption } = await prompt(homeOptions);
 
   if (selectedOption === 'Exit') {
-    // exit = true;
-    console.log('Exiting the App...');
+    console.info('Exited Terminal Chat App Successfully!');
     process.exit(0);
   } else if (selectedOption === 'Create-Chat-Room') {
-    new Separator(`${selectedOption}`);
-
+    console.log('Creating Chat Room');
+    // create chat room function
+    // ask if he wants to join the chat room he created
   } else if (selectedOption === 'Join-Chat-Room') {
-    new Separator(`${selectedOption}`);
+    console.log('Joining...');
+    // list the created chat rooms
+    // should be able to select any of the chat rooms
   }
 
-
-  // When an option is selected. Only the Home and exit button should be visible
-  // when the home button is selected the options should be re-rendered
-  // when the exit button is selected the app should exit
-
-  // new Separator(), { name: 'Home', value: 'Home', message: 'Go back to Home'
-
-  //   while (!exit) {
-  //     const { selectedOption } = await prompt({
-  //       type: 'list',
-  //       name: 'selectedOption',
-  //       message: 'Home',
-  //       choices: [...options],
-  //     });
-
-  //     // if (selectedOption === 'Home') {
-  //     //   continue;
-  //     // } else 
-  //     if (selectedOption === 'Exit') {
-  //       // exit = true;
-  //       console.log('Exiting the App...');
-  //       process.exit(0);
-  //     } else if (selectedOption === 'Create-Chat-Room') {
-  //       console.log('Creating Chat Room...');
-
-  //     } else if (selectedOption === 'Join-Chat-Room') {
-  //       console.log('Joining Chat Room...');
-  //     }
-
-  //     // Additional logic for each option can be added here
-  //   }
 });
 
 program.parse(process.argv);
