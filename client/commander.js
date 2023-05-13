b01985e27279777823cda980e90d8a3c45d17471
Hayatudeen Abdulrahman <104792493+HayatsCodes@users.noreply.github.com>
2023-05-13 17:28:06 +0000

    refactored commander.js

--- client/commander.js
+++ client/commander.js
@@ -11,25 +11,25 @@ program.version('1.0.0').description('Terminal Chat App');
 
 // Start Terminal chat app
 program
-.description('Starts the Terminal chat app')
-.command('start').action(async () => {
-  // Render interface according to what the user selects
-  const selectedOption = await getMenuOption();
+  .description('Starts the Terminal chat app')
+  .command('start').action(async () => {
+    // Render interface according to what the user selects
+    const selectedOption = await getMenuOption();
 
-  if (selectedOption === 'Exit') {
-    console.info('Exited Terminal Chat App Successfully!');
-    process.exit(0);
-  } else if (selectedOption === 'Create-Chat-Room') {
-    console.log('Creating Chat Room');
-    // create chat room function
-    // ask if he wants to join the chat room he created
-  } else if (selectedOption === 'Join-Chat-Room') {
-    console.log('Joining...');
-    // list the created chat rooms
-    // should be able to select any of the chat rooms
-  }
+    if (selectedOption === 'Create-Chat-Room') {
+      console.log('Creating Chat Room');
+      // create chat room function
+      // ask if he wants to join the chat room he created
+    } else if (selectedOption === 'Join-Chat-Room') {
+      console.log('Joining...');
+      // list the created chat rooms
+      // should be able to select any of the chat rooms
+    } else if (selectedOption === 'Exit') {
+      console.info('Exited Terminal Chat App Successfully!');
+      process.exit(0);
+    }
 
-});
+  });
 
 program.parse(process.argv);
 
