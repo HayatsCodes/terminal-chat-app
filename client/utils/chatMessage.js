5830d4d2c981f7650727aee83b270e3ccb5625c1
Hayatudeen Abdulrahman <104792493+HayatsCodes@users.noreply.github.com>
2023-05-17 16:19:09 +0000

    chat message refactored

--- client/utils/chatMessage.js
+++ client/utils/chatMessage.js
@@ -11,21 +11,3 @@ module.exports = function chatMessage(client, room) {
     client.emit('chat message', room, message);
   });
 };
-
-
-
-
-
-
-  // module.exports = function chatMessage(client, room) {
-//     console.log('Setting up chat room...');
-//     try {
-//         process.stdin.on('data', (data) => {
-//             const message = data.toString().trim();
-//             console.log('message');
-//             client.emit('chat message', room, message);
-//         });
-//     } catch (error) {
-//         console.error('Error setting up process.stdin:', error);
-//     }
-// }
