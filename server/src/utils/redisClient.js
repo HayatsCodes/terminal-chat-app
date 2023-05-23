783f1f1e88fad49e5ec9a4a27de4e7ac1dde4bd6
Hayats <hayatudeenabdulrahman@gmail.com>
2023-05-23 21:43:49 +0100

    set up redis for production

--- server/src/utils/redisClient.js
+++ server/src/utils/redisClient.js
@@ -1,7 +1,18 @@
 const { createClient } = require('redis');
+let redisClient;
 
-// Create a Redis client
-const redisClient = createClient();
+if (process.env.NODE_ENV === 'production') {
+    // Create a Redis client with the production redis url
+    redisClient = createClient({
+        url: `${process.env.REDIS_URL}`
+    });
+    
+} else {
+    // Create a Redis client with the default port
+    redisClient = createClient();
+}
+
+
 
 
 redisClient.on('error', err => console.log('Redis Client Error', err));
